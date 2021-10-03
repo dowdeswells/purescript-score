546a6ac02e85c4ecd236276763947b418a21a4d2
simon <dowdeswells@yahoo.com.au>
2021-10-03 15:21:30 +1100

    remove the corefn arg to purs compiler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./index.css');
 if (process.env.NODE_ENV == "production") {
-  require("./dist/Main").main();
+  require("./output/Main").main();
 } else {
   require("/output/Main").main();
 }
